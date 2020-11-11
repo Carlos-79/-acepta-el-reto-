9b1d73aa3c0b397a74d439671f0a4daa416bd82b
Carlos-79 <56670771+Carlos-79@users.noreply.github.com>
2020-11-11 11:08:01 +0100

    Add files via upload

---
+++ AceptaelReto380.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AceptaelReto380 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int np = sc.nextInt();
+		int ng = sc.nextInt();
+		int total
+		
+		while( total < 50.000) {
+			
+			total = ng + ng
+		
+			System.out.println(+total);
+		 np  = sc.nextInt();
+		 ng = sc.nextInt();
+			
+		}
+	}
+
+}
