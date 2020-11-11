11c3d3388c9a618f52d41fc971cc31e7617eecb8
Carlos-79 <56670771+Carlos-79@users.noreply.github.com>
2020-11-11 10:23:36 +0100

    Add files via upload

---
+++ pr155perimetrodeunrectangulo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pr155perimetrodeunrectangulo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// la fórmula del perímetro es p = 2*(altura +base) 
+		int altura = sc.nextInt();
+		int base = sc.nextInt();
+		int perí
+		if () {
+			
+			
+			
+		
+		}
+}
+}
